f23198af83e6b603e24d027e0bd0e0eade880bec
Selin Su Varol <selinsuvarol@gmail.com>
2022-04-11 22:20:21 +0300

    completed the assignment, this was very challenging :D

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Selin',
+    streetAddress: 'Istanbul',
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+    };
+};
+// console.log(updateEmployeeWithKeyAndValue(employee, "key2", "1"));
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]= value;
+    return obj;
+};
+// console.log(updateEmployeeWithKeyAndValue(employee, "name", "Beyza"));
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+};
+// console.log(updateEmployeeWithKeyAndValue(employee, "name",));
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key];
+    return obj;
+};
